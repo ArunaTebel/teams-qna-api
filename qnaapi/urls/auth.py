599b8bcbae6f79fab720d426740363ff67eae62d
ArunaTebel <arunatebel@hotmail.com>
2020-04-19 14:45:55 +0530

    adding the trailing slash to the current-user endpoint

--- qnaapi/urls/auth.py
+++ qnaapi/urls/auth.py
@@ -4,5 +4,5 @@ from qnaapi import views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('auth/current-user', views.auth.CurrentUser.as_view(), name='current_user'),
+    path('auth/current-user/', views.auth.CurrentUser.as_view(), name='current_user'),
 ]
